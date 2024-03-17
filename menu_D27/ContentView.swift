6fa72e97bbfdc92d3aeec3478071c145ca5e59db
sirx2713 <bradx2713@gmail.com>
2024-03-17 07:54:24 +0300

    Initial Commit

---
+++ menu_D27/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  menu_D27
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/03/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ menu_D27/menu_D27App.swift
@@ -0,0 +1,17 @@
+//
+//  menu_D27App.swift
+//  menu_D27
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/03/17.
+//
+
+import SwiftUI
+
+@main
+struct menu_D27App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
